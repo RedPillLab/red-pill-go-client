a0ddcc0362740ba8b68936bf81bb50edc84835fa
you74674 <you74674@gmail.com>
2022-10-18 14:11:24 +0800

    add more comments and description

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # RedPillGo client example
 
+- main.js contains the example of receiving motion from webrtc and retargeting it to VRM model.  
+	brief description of the content:
+	- pick a room id and connect WebRTCClient.Client to signaling server
+		- make sure to connect RedPillGo to the same room
+	- receive data from the message event of WebRTCClient.Client
+	- use Parser.parse to parse data received from WebRTCClient.Client.
+	- load VRM model using three-vrm
+	- register retarget skeleton with retargeter.scanBones(VRMSkeleton.getBones(vrm)).
+	- set the motion data to retargeter using retargeter.setRotations and retargeter.setTranslation.
+		- set morph data using vrm.expressionManager.setValue
+	- call retargeter.update and vrm.update(delta) in render loop.
+
+## run
 - download [example VRM model](https://github.com/pixiv/three-vrm/blob/dev/packages/three-vrm/examples/models/VRM1_Constraint_Twist_Sample.vrm) and put it in this directory
 - run http-server
 - browse http://localhost:8080
--- main.js
+++ main.js
@@ -13,15 +13,19 @@ const WS_URL = "wss://webrtc.rplab.online/";
 class App{
 	constructor(){
 		this.engine = new Engine(document.getElementById("three"));
+		//webrtc client
 		this.client = new Client();
 
+		//listen to webrtc message
 		this.client.addEventListener("message", (e)=>{
+			//parse and send the motion data to model
 			let [type, data] = Parser.parse(e.data);
 			this.model?.dispatchEvent({type: type, data: data});
 		});
 		this.initGUI();
 
 		(async()=>{
+			//load vrm and add to threejs main scene
 			const [vrm, script] = await this.loadVRM('VRM1_Constraint_Twist_Sample.vrm');
 			this.vrm = vrm;
 			this.model = vrm.scene;
@@ -61,30 +65,33 @@ class App{
 	//VRM
 	async loadVRM(path){
 		const loader = new GLTFLoader();
-		const helperRoot = new THREE.Group();
-		helperRoot.renderOrder = 10000;
+		// const helperRoot = new THREE.Group();// helperRoot is for debugging only
+		// helperRoot.renderOrder = 10000;
 		loader.register( parser => new THREE_VRM.VRMLoaderPlugin(parser, {
-			helperRoot: helperRoot,
-			autoUpdateHumanBones: true
-		}) );
+			// helperRoot: helperRoot
+		}));
 
+		// loading
 		const gltf = await loader.loadAsync(path);
 		THREE_VRM.VRMUtils.removeUnnecessaryVertices( gltf.scene );
 		THREE_VRM.VRMUtils.removeUnnecessaryJoints( gltf.scene );
 		const vrm = gltf.userData.vrm;
-		// THREE_VRM.VRMUtils.rotateVRM0( vrm );
-		if (vrm.meta?.metaVersion === '0') 
-			vrm.humanoid.getRawBoneNode("hips").rotation.y = Math.PI;
-		
 		const model = vrm.scene;
 		// model.add(helperRoot);
+		// THREE_VRM.VRMUtils.rotateVRM0( vrm );
+		if (vrm.meta?.metaVersion === '0')//this is neccessary since old VRM spec is different
+			vrm.humanoid.getRawBoneNode("hips").rotation.y = Math.PI;
+
+		// prepare retargeter
 		const retargeter = new Retargeter();
 		retargeter.scanBones(VRMSkeleton.getBones(vrm));
 
+		// set morph
 		model.addEventListener("morph", (e)=>{
 			for(const [key, value] of Object.entries(VRMSkeleton.morph_map))
 				vrm.expressionManager.setValue(key, e.data[value]);
 		});
+		// set motion data to retargeter
 		model.addEventListener("rotations", (e)=>{
 			retargeter.setRotations(e.data);
 		});
@@ -92,9 +99,9 @@ class App{
 			retargeter.setTranslation(...e.data);
 		});
 
-		return [vrm, (time)=>{
+		return [vrm, (delta)=>{//this will be called in render loop
 			retargeter.update();
-			vrm.update(time);
+			vrm.update(delta);
 		}];
 	}
 }
